36d6639ad07f60d3f1f4b5cac537503d380edfe6
dev@ <digiskills2525@gmail.com>
2025-08-06 20:52:24 +0500

    project setup and create gitignore and add those file which are not allowed for version control system

--- instagram_project/settings.py
+++ instagram_project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'instagram_app',
 ]
+
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -74,11 +77,15 @@ WSGI_APPLICATION = 'instagram_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'instagram_clone',     
+        'USER': 'postgres',
+        'PASSWORD': 'codenzy123',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
